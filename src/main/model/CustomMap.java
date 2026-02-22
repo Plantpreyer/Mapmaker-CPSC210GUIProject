48546d7896ae57f53e230122bb293c4521932800
Sijing Liu <sijingliu08@gmail.com>
2026-02-22 14:45:16 -0800

    customMap class (incomplete)

--- src/main/model/CustomMap.java
+++ src/main/model/CustomMap.java
@@ -1,16 +1,24 @@
 package model;
 
+import java.util.*;
+
+import model.feature.MapObject;
+import model.feature.Marker;
 
 // represents a customiizable map with buildings, roads, trees, routes, and markers
 // map has a name
 public class CustomMap {
     String name;
+    List<MapObject> objects;
+    List<Marker> markers;
 
     // REQUIRES: name not empty
     // MODIFIES: this
     // EFFECTS: constructor
     public CustomMap(String name) {
         this.name = name;
+        this.objects = new ArrayList<>();
+        this.markers = new ArrayList<>();
     }
 
 }
